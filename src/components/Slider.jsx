2ba049d14e0aec5e0c73472db2a67b3c7db34434
Tatiana Borisova <taniaborisovafr@gmail.com>
2024-08-05 13:13:56 +0200

    les flèches de navigation et le numéro de slide sont invisibles si le slide contient une seule image

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,6 +5,7 @@ import arrowRight from '../assets/Icones/FlècheDroite.png'
 
 const Slider = ({ pictures }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const hasMultiplePictures = pictures.length > 1;
 
   const goToPrevious = () => {
     setCurrentIndex((prevIndex) =>
@@ -20,20 +21,26 @@ const Slider = ({ pictures }) => {
 
   return (
     <div className="slider">
+       {hasMultiplePictures && (
       <button className="slider-button prev" onClick={goToPrevious}>
       <img src={arrowLeft} alt="Previous" className="arrow-icon" />
       </button>
+       )}
       <img
         src={pictures[currentIndex]}
         alt={`Slide ${currentIndex + 1}`}
         className="slide-image"
       />
+      {hasMultiplePictures && (
       <div className="slide-number">
         {currentIndex + 1}/{pictures.length}
       </div>
+           )}
+           {hasMultiplePictures && (
       <button className="slider-button next" onClick={goToNext}>
       <img src={arrowRight} alt="Next" className="arrow-icon" />
       </button>
+           )}
     </div>
   );
 };
